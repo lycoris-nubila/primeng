55e66c22a499d8f6ca1103cf22c3eb71c79bf05f
sdoubey <sebastien.doubey@gmail.com>
2020-09-28 20:09:25 +0200

    Fix calendar onPush

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -2106,6 +2106,7 @@ export class Calendar implements OnInit,OnDestroy,ControlValueAccessor {
 
         this.updateInputfield();
         this.updateUI();
+        this.cd.markForCheck();
     }
     
     registerOnChange(fn: Function): void {
